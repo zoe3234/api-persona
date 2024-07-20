2eb103a097f64db2302140e3dcfe1512e199494d
zoe <zoecardosoibarra@gmail.com>
2024-07-20 19:42:04 -0300

    arreglar nombre de tabla a personas

--- api-persona/app/Models/Persona.php
+++ api-persona/app/Models/Persona.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Persona extends Model
 {
     use HasFactory;
-    protected $table = 'persona';
+    protected $table = 'personas';
     protected $fillable = [
         'id',
         'nombre',
