807dcf27f4b81fb1ead139760c6c429671a3278c
zoe <zoecardosoibarra@gmail.com>
2024-07-20 22:54:42 -0300

    rutas basicas

--- api-persona/routes/api.php
+++ api-persona/routes/api.php
@@ -2,18 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PersonaController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/personas', [PersonaController::class, 'listar']);
+Route::post('/personas', [PersonaController::class, 'alta']);
+Route::put('/personas/{id}', [PersonaController::class, 'modificar']);
+Route::delete('/personas/{id}', [PersonaController::class, 'baja']);
+Route::get('/personas/{id}', [PersonaController::class, 'buscar']);
